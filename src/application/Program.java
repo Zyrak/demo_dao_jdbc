a99aaaffdeb0413f7098f4aa0af8b07694ac27b2
Zyrak <zyrak.br@gmail.com>
2024-03-01 12:21:55 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,15 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("=== Test 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		seller.setEmail("martha@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		
 	}
 
 }
